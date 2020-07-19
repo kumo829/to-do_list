02396584929ba85994d6768c38ae9ce02b072efd
Alex Montoya <amonmar2000@hotmail.com>
2020-07-19 06:59:18 -0500

    Initial upload of tasks-service to the repository with custom .gitignore file.

---
+++ lists-service/src/test/java/com/javatutoriales/todolist/listsservice/ListsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatutoriales.todolist.listsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
