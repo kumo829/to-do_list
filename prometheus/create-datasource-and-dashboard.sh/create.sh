f067ca6f6176da9200e7dcc1f6e70ea1dcd849d1
Alex Montoya <amonmar2000@hotmail.com>
2020-08-29 09:37:04 -0500

    Updated zipkin files to register metrics on Grafana through Prometheus

--- prometheus/create-datasource-and-dashboard.sh/create.sh
+++ prometheus/create-datasource-and-dashboard.sh/create.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -xeuo pipefail
 
-if ! curl --retry 5 --retry-connrefused --retry-delay 0 -sf http://grafana:3000/api/dashboards/name/prom; then
+if ! curl --retry 5 --retry-connrefused --retry-delay 0 -sf http://grafana:3001/api/dashboards/name/prom; then
     curl -sf -X POST -H "Content-Type: application/json" \
          --data-binary '{"name":"prom","type":"prometheus","url":"http://prometheus:9090","access":"proxy","isDefault":true}' \
-         http://grafana:3000/api/datasources
+         http://grafana:3001/api/datasources
 fi
 
 dashboard_id=1598
@@ -15,4 +15,11 @@ curl -s https://grafana.com/api/dashboards/${dashboard_id}/revisions/${last_revi
     xargs -0 -I "{}" curl --retry-connrefused --retry 5 --retry-delay 0 -sf \
           -X POST -H "Content-Type: application/json" \
           --data-binary '{"dashboard": {}, "inputs": [{"name": "DS_PROM", "pluginId": "prometheus", "type": "datasource", "value": "prom"}], "overwrite": false}' \
-          http://grafana:3000/api/dashboards/import
+          http://grafana:3001/api/dashboards/import
+
+
+curl -s https://grafana.com/api/dashboards/1598/revisions/15/download | \
+    xargs -0 -I "{}" curl --retry-connrefused --retry 5 --retry-delay 0 -sf \
+          -X POST -H "Content-Type: application/json" \
+          --data-binary '{"dashboard": {}, "inputs": [{"name": "DS_PROM", "pluginId": "prometheus", "type": "datasource", "value": "prom"}], "overwrite": false}' \
+          http://localhost:3001/api/dashboards/import
